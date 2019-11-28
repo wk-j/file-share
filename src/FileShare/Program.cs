2b1b9916bb31ccc24b382206ff48f3c1df35435d
wk-j <somnuk.wk@gmail.com>
2019-11-29 00:50:49 +0700

    Refactor

--- src/FileShare/Program.cs
+++ src/FileShare/Program.cs
@@ -6,26 +6,20 @@ using System.Text;
 namespace FileShare {
     class Program {
         static void Connect(String server, int port, String message) {
-            try {
-                // Send
-                var data = Encoding.ASCII.GetBytes(message);
-                var client = new TcpClient(server, port);
-                var stream = client.GetStream();
-                stream.Write(data, 0, data.Length);
+            // Send
+            var data = Encoding.ASCII.GetBytes(message);
+            var client = new TcpClient(server, port);
+            var stream = client.GetStream();
+            stream.Write(data, 0, data.Length);
 
-                // Receive
-                var temp = new Byte[256];
-                var bytes = stream.Read(temp, 0, temp.Length);
-                var responseData = Encoding.ASCII.GetString(temp, 0, bytes);
-                Console.WriteLine("{0}", responseData);
+            // Receive
+            var temp = new Byte[256];
+            var bytes = stream.Read(temp, 0, temp.Length);
+            var responseData = Encoding.ASCII.GetString(temp, 0, bytes);
+            Console.WriteLine(responseData);
 
-                stream.Close();
-                client.Close();
-            } catch (ArgumentNullException e) {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            } catch (SocketException e) {
-                Console.WriteLine("SocketException: {0}", e);
-            }
+            stream.Close();
+            client.Close();
         }
 
         static void Main(string[] args) {
